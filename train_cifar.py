19d2670e23b78d60879965f7a1cad26f9a12111d
lordwarlock <daniel_zzh1991@hotmail.com>
2019-03-06 15:18:21 -0500

    training hyperpara

--- train_cifar.py
+++ train_cifar.py
@@ -7,17 +7,23 @@ import argparse
 import tqdm
 
 def load_train_test(args):
-    transform = transforms.Compose(
+    transform_train = transforms.Compose(
+        [transforms.RandomCrop(32, padding=4),
+         transforms.RandomHorizontalFlip(),
+         transforms.ToTensor(),
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+    transform_test = transforms.Compose(
         [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])   
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
+                                            download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
                                               shuffle=True, num_workers=2)  
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
+                                           download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size,
                                              shuffle=False, num_workers=2)  
 
@@ -26,7 +32,7 @@ def load_train_test(args):
 
     return trainloader, testloader
 
-def train(epoch, trainloader, net, criterion, optimizer, args):
+def train(epoch, trainloader, net, criterion, optimizer, args, lr):
     running_loss = 0.0
     pbar = tqdm.tqdm(enumerate(trainloader, 0))
     for i, data in pbar:
@@ -48,8 +54,8 @@ def train(epoch, trainloader, net, criterion, optimizer, args):
         # print statistics
         running_loss += loss.item()
         if i % args.log_interval == args.log_interval - 1:    # print every 2000 mini-batches
-            pbar.set_description('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / args.log_interval))
+            pbar.set_description('[%d, %5d, %.3f] loss: %.3f' %
+                  (epoch + 1, i + 1, lr, running_loss / args.log_interval))
             running_loss = 0.0
 
 def test(testloader, net, args):
@@ -110,7 +116,7 @@ def parse_args():
                         help='random seed (default: 1234)')
     parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--weight-decay', type=float, default=0., metavar='WD',
+    parser.add_argument('--weight-decay', type=float, default=5e-4, metavar='WD',
                         help='weigth decay')
     parser.add_argument('--lr-decay-epoch', type=int, default=100, metavar='LDE',
                         help='number of epochs to decay lr')
@@ -131,6 +137,7 @@ def adjust_learning_rate(optimizer, epoch, args):
     lr = args.lr * (0.1 ** (epoch // args.lr_decay_epoch))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+    return lr
 
 def main():
     args = parse_args()
@@ -148,10 +155,12 @@ def main():
         #net.cuda()
         net = torch.nn.DataParallel(net).cuda()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                                            momentum=args.momentum,
+                                            weight_decay=args.weight_decay)
     for epoch in range(args.epochs):
-        adjust_learning_rate(optimizer, epoch, args)
-        train(epoch, trainloader, net, criterion, optimizer, args)
+        lr = adjust_learning_rate(optimizer, epoch, args)
+        train(epoch, trainloader, net, criterion, optimizer, args, lr)
         test(testloader, net, args)
         if args.save_path != '':
             torch.save(net.module.state_dict(), args.save_path)
